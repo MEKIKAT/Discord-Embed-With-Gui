b573eef5168aebded85d4bc66b66d3247078cea3
Paka <73528514+MEKIKAT@users.noreply.github.com>
2022-10-21 00:32:29 +0800

    awd
    awd

--- Discord Embed.py
+++ Discord Embed.py
@@ -2,70 +2,46 @@ from discord_webhook import DiscordEmbed, DiscordWebhook
 from tkinter.font import BOLD
 from tkinter import *
 
-
 bgc = "#464646"
 
-def server(urll):      
-    web = DiscordWebhook(url=urll, username=eNamebot.get(), content="@"+eOverMessage.get())    
+def server():      
+    web = DiscordWebhook(url="https://discord.com/api/webhooks/1032315409652781146/v0yQdnJ0nhmWrgUj-6KD1QMhIvd1CTBRjqUeCW827My7HtrcwtoGf-JnYmtedV7lhPD-", username="The Seekers  Lair PH | General Rules ", content=eOverMessage.get())    
     
-    embed = DiscordEmbed(title=eTitle.get())
+    embed = DiscordEmbed(color='D3D3D3')
     embed.set_author(name=eEmbedUser.get() ,icon_url=eIcon.get())
-    embed.add_embed_field(name=eTitle.get(),value=eDetails.get(1.0,END))
+    embed.add_embed_field(name="Sector I.2",value=eDetails.get(1.0,END))
+    #embed.set_image(url=eIcon.get())
 
+    #embed.color(color='#1ABC9C')
     web.add_embed(embed)
     response = web.execute()
      
-
-
+     
 root = Tk()
 #root.geometry("600x450")
 root.configure(background=bgc)
-
 mainframe = LabelFrame(root, text="Main Frame",pady=10,padx=50,bg = bgc)
-
 appHeader = Label(mainframe, text="Discord Webhook", font=('Poiret One', 20, BOLD),bg = bgc)
-
-txtHk = Label(mainframe,text="Weebhook",bg = bgc)#
+txtHk = Label(mainframe,text="Weebhook",bg = bgc)
 eWebhook = Entry(mainframe)
-
-txtBT = Label(mainframe,text="Bot Name",bg = bgc)#
+txtBT = Label(mainframe,text="Bot Name",bg = bgc)
 eNamebot = Entry(mainframe)
-
-txtOM = Label(mainframe,text="Message",bg = bgc)#
+txtOM = Label(mainframe,text="Message",bg = bgc)
 eOverMessage = Entry(mainframe)
-
 Embedframe = LabelFrame(root, text="Embed Field",pady=10,padx=60,bg = bgc)
-
 txtTitle = Label(Embedframe,text="Content Title",bg = bgc)#
 eTitle = Entry(Embedframe)
-
 txtEmbedUser = Label(Embedframe,text="User Name",bg = bgc)#
 eEmbedUser = Entry(Embedframe)
-
 txtIconUser = Label(Embedframe,text="Icon User",bg = bgc)#
 eIcon = Entry(Embedframe)
-
-txtDetails = Label(Embedframe,text="Details",bg = bgc)#
-eDetails = Text(Embedframe,height=15,width=50)
-
-submitBtn = Button(Embedframe,text="Submit",command=lambda: server(eWebhook.get()),bg = bgc)
+txtDetails = Label(Embedframe,text="Details",bg = bgc , )#
+eDetails = Text(Embedframe,height=15,width=80)
+submitBtn = Button(Embedframe,text="Submit",command=server,bg = bgc)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-###
 mainframe.grid(row=0, column=0,padx=10,pady=10)
 appHeader.grid(row=0, column=0,columnspan=3)
 txtHk.grid(row=1,column=0)
@@ -84,6 +60,7 @@ eIcon.grid(row=1,column=2)
 txtDetails.grid(row=2,columnspan=3)
 eDetails.grid(row=3,column=0,columnspan=3)
 submitBtn.grid(row=4,columnspan=3)
+
 #server()
 root.resizable(False,False)
 root.mainloop()  
